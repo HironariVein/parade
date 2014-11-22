ce703a33cf4e62c4c6ad9e77c1420e66f2ee58df
Yamashita <hiroyama1124@gmail.com>
2014-11-22 22:50:25 +0900

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parade::Application.config.secret_key_base = '74ed347b8505ef78634cf91d3f4161a4720f29c82385e194617658ab3585038a3b47fea8a38a3b4890b2a68b5f079fc8b1899d8380a042fd8dc725279ebbcc58'
